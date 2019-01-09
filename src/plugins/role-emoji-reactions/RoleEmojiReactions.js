6ef6d539dd4f7ca4b8fddee349b17b259857166b
Rene <kenavr@gmail.com>
2019-01-09 13:50:42 +0100

    Updated RoleEmojiReactions

--- src/plugins/role-emoji-reactions/RoleEmojiReactions.js
+++ src/plugins/role-emoji-reactions/RoleEmojiReactions.js
@@ -1,25 +1,48 @@
 const EmojiReaction = require('../emoji-reactions');
 
 // Default options to illustrate shape
-const initialOptions = {
+/* const initialOptions = {
   channel: '',
   messageId: '',
   rules: {
     emojiName: 'role',
     emojiName2: 'role2'
   }
-};
+};*/
+
+function RoleEmojiReactionsParams(client, options) {
+  this.client = client;
+  this.options = options;
+}
 
 function RoleEmojiReactions(client, options) {
-  function handleReaction(emoji, user) {
-    function assignRole(user, role) {}
+  function validateParams(reactionParams) {
+    const { client } = reactionParams;
+
+    if (!client) {
+      throw new Error('RoleEmojiReactions: No discord client provided!');
+    }
+
+    return Promise.resolve(reactionParams);
   }
 
-  function init() {
+  function handleReaction(emoji, user) {
+    function assignRole(user, role) {
+      if (!user.roles.some(r => r.name === role)) {
+        user.addRole(role);
+      }
+    }
+
+    assignRole(user, options.rules[emoji]);
+  }
+
+  function init(reactionParams) {
+    const { client, options } = reactionParams;
     new EmojiReaction(client, handleReaction, options);
   }
 
-  init();
+  const params = new RoleEmojiReactionsParams(client, options);
+  validateParams(params).then(init);
 }
 
 module.exports = RoleEmojiReactions;
